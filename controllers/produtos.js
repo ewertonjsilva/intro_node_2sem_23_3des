36072e57e08a95074457d7fb38e1441d7130e23c
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-09-05 13:39:16 -0300

    controller usuarios para exemplo

--- controllers/produtos.js
+++ controllers/produtos.js
@@ -31,7 +31,6 @@ module.exports = {
             const values = [prd_nome, prd_valor, prd_unidade, ptp_id, prd_disponivel, prd_img, prd_destaque, prd_img_destaque, prd_descricao, prd_id];    
             const atualizacao = await db.query(sql, values);
             return response.status(200).json({confirma: 'Sucesso', message: 'Dados atualizados', "itensAtualizados": atualizacao[0].affectedRows});            
-
         } catch (error) { 
             return response.status(500).json({confirma: 'Erro', message: error});
         }        
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -14,14 +14,30 @@ module.exports = {
     }, 
     async cadastrarUsuarios(request, response) {
         try {
-            return response.status(200).json({confirma: 'Cadastrar Usuarios'});
+            const { usu_nome, usu_email, usu_senha, usu_tipo, usu_ativo } = request.body;
+            const sql = 'INSERT INTO usuarios (usu_nome, usu_email, usu_senha, usu_tipo, usu_ativo) VALUES (?, ?, ?, ?, ?);';
+            const values = [usu_nome, usu_email, usu_senha, usu_tipo, usu_ativo];
+            const confirmacao = await db.query(sql, values); 
+            const usu_id = confirmacao[0].insertId;
+            return response.status(200).json({confirma: 'Cadastro de produto realizado com sucesso', message: usu_id});
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
     }, 
     async editarUsuarios(request, response) {
         try {
-            return response.status(200).json({confirma: 'Editar Usuarios'});
+            // parâmetros recebidos pelo corpo da requisição
+            const { usu_nome, usu_email, usu_senha, usu_tipo, usu_ativo } = request.body;
+            // parâmetro recebido pela URL via params (valor direto na url ex: /usuarios/1). 
+            const { usu_id } = request.params; 
+            // instrução SQL para atualização dos valores
+            const sql = 'UPDATE usuarios SET usu_nome = ?, usu_email = ?, usu_senha = ?, usu_tipo = ?, usu_ativo = ? WHERE usu_id = ?;';  
+            // preparo do array com dados que serão atualizados
+            const values = [usu_nome, usu_email, usu_senha, usu_tipo, usu_ativo, usu_id];    
+            // execução e obtenção de confirmação da atualização realizada
+            const atualizacao = await db.query(sql, values);
+            // reponde a requisição com a mensagem confirmando o nº de registros atualizados
+            return response.status(200).json({confirma: 'Sucesso', message: 'Dados atualizados', "registrosAtualizados": atualizacao[0].affectedRows});         
         } catch (error) {
             return response.status(500).json({confirma: 'Erro', message: error});
         }
